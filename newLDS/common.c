c2fce51395b98a001d5378d4384b224729220f86
ssorry123 <syd03160@gmail.com>
2020-09-21 23:34:51 +0900

    공통 파일

---
+++ newLDS/common.c
@@ -0,0 +1,6 @@
+#pragma once
+#include "common.h"
+
+void errmsg(char* str) {
+	printf("log : err or fail in %s\n", str);
+}
---
+++ newLDS/common.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+#include <stdio.h>
+// 모든 모듈에 공통적으로 사용되는 상수나 자료형, 헤더파일을 선언합니다.
+
+#define MAXSIZE 200000000	// 2억
+// 데이터 영역(전역변수)에서 배열의 크기는 10억byte = 1GB를 넘길 수 없음
+// MAXSIZE가 2억이고, 4byte int 배열 하나 선언한다면? 8억byte 약 800MB 사용
+
+#define FAIL -1
+
+// use only for item
+typedef int32_t myint;
+
+// print error message
+void errmsg(char* str);
