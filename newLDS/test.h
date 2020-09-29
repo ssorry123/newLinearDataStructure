0885b8986964609d5ce315ef04f390b738c483e2
ssorry123 <syd03160@gmail.com>
2020-09-29 18:24:19 +0900

    txt파일로 로그 남기도록 수정

--- newLDS/test.h
+++ newLDS/test.h
@@ -5,43 +5,48 @@
 
 
 void test_create_destroy(int size) {
-    printf("start test create and destroy\n");
+    FILE* fp;
+    fp = fopen("../test/test_create_destroy.txt", "w");
+    fprintf(fp, "start test create and destroy\n");
     Sleep(3000);
 
-    printf("create array list\n");
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(size);
     Sleep(3000);
 
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(size);
     Sleep(3000);
 
     // check all zero
-    print_array_list_all(al);
-    print_linked_list_all(ll);
+    print_array_list_all(fp, al);
+    print_linked_list_all(fp, ll);
 
     // 메모리 감소 확인
-    printf("destroy array \n");
+    fprintf(fp, "destroy array \n");
     destroy_array_list(al);
     Sleep(3000);
 
-    printf("destroy linked\n");
+    fprintf(fp, "destroy linked\n");
     destroy_linked_list(ll);
     Sleep(3000);
 
     // end
-    printf("end test create and destroy\n");
+    fprintf(fp, "end test create and destroy\n");
     Sleep(3000);
+    fclose(fp);
     return;
 }
 
 void test_seq_change(int size) {
+    FILE* fp;
+    fp = fopen("../test/test_seq_change.txt", "w");
     // 연결리스트의 경우
     // 해당 idx의 값을 바꿀때, change 함수 사용
     // (v.0.1에서 제공되는 인터페이스만 사용한다)
-    printf("create array list\n");
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(size);
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(size);
 
     for (int i = 0; i < size; ++i) {
@@ -50,24 +55,27 @@ void test_seq_change(int size) {
     }
 
     for (int i = 0; i < size; ++i) {
-        printf("[%5d] == %5d, %5d\n", i, at_array_list(al, i), at_linked_list(ll, i));
+        fprintf(fp, "[%5d] == %5d, %5d\n", i, at_array_list(al, i), at_linked_list(ll, i));
     }
 
-    printf("destroy array \n");
+    fprintf(fp, "destroy array \n");
     destroy_array_list(al);
-    printf("destroy linked\n");
+    fprintf(fp, "destroy linked\n");
     destroy_linked_list(ll);
 
-    printf("end test\n");
+    fprintf(fp, "end test\n");
+    fclose(fp);
 }
 
 void test_insert(int size) {
-    printf("create array list\n");
+    FILE* fp = fopen("../test/test_insert.txt", "w");
+
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(size);
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(size);
 
-    printf("init arr[idx] = idx\n");
+    fprintf(fp, "init arr[idx] = idx\n");
     for (int i = 0; i < size; ++i) {
         change_array_list(al, i, i);
         change_linked_list(ll, i, i);
@@ -85,20 +93,24 @@ void test_insert(int size) {
     insert_array_list(al, 0, 111111);
     insert_linked_list(ll, 0, 111111);
 
-    print_array_list_all(al);
-    print_linked_list_all(ll);
+    print_array_list_all(fp, al);
+    print_linked_list_all(fp, ll);
 
     destroy_array_list(al);
     destroy_linked_list(ll);
 
-    printf("end test");
+    fprintf(fp, "end test");
+
+    fclose(fp);
 }
 
 void test_insert2(int size) {
+    FILE* fp = fopen("../test/test_insert2.txt", "w");
+
     // 하나라도 존재해야 삽입할 수 있다
-    printf("create array list\n");
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(1);
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(1);
 
 
@@ -106,40 +118,45 @@ void test_insert2(int size) {
         insert_array_list(al, 0, i);
         insert_linked_list(ll, 0, i);
     }
-    print_array_list_all(al);
-    print_linked_list_all(ll);
+    print_array_list_all(fp, al);
+    print_linked_list_all(fp, ll);
 
     destroy_array_list(al);
     destroy_linked_list(ll);
 
-    printf("end test");
+    fprintf(fp, "end test");
+    fclose(fp);
 }
 
 void test_append(int size) {
+    FILE* fp = fopen("../test/test_append.txt", "w");
     // 빈 리스트 생성
-    printf("create array list\n");
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(0);
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(0);
 
     for (int i = 0; i < size; ++i) {
-        append_array_list(al, i+2);
+        append_array_list(al, i + 2);
         append_linked_list(ll, i + 2);
     }
 
-    print_array_list_all(al);
-    print_linked_list_all(ll);
+    print_array_list_all(fp, al);
+    print_linked_list_all(fp, ll);
 
     destroy_array_list(al);
     destroy_linked_list(ll);
 
-    printf("end test");
+    fprintf(fp, "end test");
+
+    fclose(fp);
 }
 
 void test_remove(int size) {
-    printf("create array list\n");
+    FILE* fp = fopen("../test/test_remove.txt", "w");
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(size);
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(size);
 
     for (int i = 0; i < size; ++i) {
@@ -157,19 +174,21 @@ void test_remove(int size) {
     remove_linked_list(ll, ll->size - 2);
     remove_linked_list(ll, ll->size - 1);
 
-    print_array_list_all(al);
-    print_linked_list_all(ll);
+    print_array_list_all(fp, al);
+    print_linked_list_all(fp, ll);
 
     destroy_array_list(al);
     destroy_linked_list(ll);
 
-    printf("end test");
+    fprintf(fp, "end test");
+    fclose(fp);
 }
 
 void test_remove2(int size) {
-    printf("create array list\n");
+    FILE* fp = fopen("../test/test_remove2.txt", "w");
+    fprintf(fp, "create array list\n");
     mAL* al = create_array_list(size);
-    printf("create linked list\n");
+    fprintf(fp, "create linked list\n");
     mLL* ll = create_linked_list(size);
 
     for (int i = 0; i < size; ++i) {
@@ -185,20 +204,21 @@ void test_remove2(int size) {
             ++i;
         }
     }
-    print_array_list_all(al);
+    print_array_list_all(fp, al);
 
     for (int i = 0; i < ll->size;) {
-        if (at_linked_list(ll , i) % 2 == 0) {
+        if (at_linked_list(ll, i) % 2 == 0) {
             remove_linked_list(ll, i);
         }
         else {
             ++i;
         }
     }
-    print_linked_list_all(ll);
+    print_linked_list_all(fp, ll);
 
     destroy_array_list(al);
     destroy_linked_list(ll);
 
-    printf("end test");
+    fprintf(fp, "end test");
+    fclose(fp);
 }
